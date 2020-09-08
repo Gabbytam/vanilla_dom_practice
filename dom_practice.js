0392e023a58183f300b588adae69b564f23475e0
Gabbytam <gtam2@fordham.edu>
2020-09-07 20:56:59 -0700

    create an element and append it

--- dom_practice.js
+++ dom_practice.js
@@ -17,4 +17,19 @@ console.log(divs);
 //changing style (color) of an existing element using .style.color and setting it equal to color of choice, in ''
 const listItems= document.querySelectorAll('li'); //need query selector ALL so that we can later choose, using [] and index number for the specific item
 listItems[2].style.color= '#6495ED'; //cornflower blue 
+//change inner text of an element using .innerText
 listItems[4].innerText= 'Jamboree sandwich at the Jambo-ree';
+
+//create a NEW element 
+//ex. create an image element, set to variable and use document.createElement('__elementtagname__')
+const newPic= document.createElement('img');
+//to give source for the new img element, use setAttribute and giving it a source
+newPic.setAttribute('src', 'https://i.pinimg.com/originals/7b/88/c0/7b88c0a83d7cba04b5890e024a05ba28.jpg');
+//to add element to your page, append it to an existing element, that is set to a variable, using appendChild
+//console.log(divs[1]); //testing to see if you can gain access to second element set to variable divs
+divs[1].appendChild(newPic);
+
+//my attempt at the bonus: get all list items to change to color of choice
+for(let i=0; i<listItems.length; i++){
+    listItems[i].style.color= '#FFA07A'; //light salmon color
+}
